4ca8f28223cad9b65b41a9dad829dc2e46efaf96
Akib <akibahmed2011@gmail.com>
2022-05-22 02:00:33 +0600

    api problem fixed

--- pages/index.js
+++ pages/index.js
@@ -100,13 +100,14 @@ export default function Home({ coins }) {
 }
 
 export const getStaticProps = async () => {
+
   const options = {
     method: 'GET',
     url: 'https://coinranking1.p.rapidapi.com/coins',
     params: {
       referenceCurrencyUuid: 'yhjMzLPhuIDl',
       timePeriod: '24h',
-      tiers: '1',
+      'tiers[0]': '1',
       orderBy: 'marketCap',
       orderDirection: 'desc',
       limit: '50',
