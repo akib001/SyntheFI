90542bf9a3ce2b240b01c568db0d8b845f9a0cf3
Akib <akibahmed2011@gmail.com>
2022-05-22 02:57:13 +0600

    updated

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
     rinkeby: {
       url: 'https://eth-rinkeby.alchemyapi.io/v2/b383d9Jv73T4vIt6yJO6y33OxhU16nB_',
       accounts: [
-        'ac262961d1752c942037656d73e9adfe5d0cffa74d8d5cb01119e11a72154389',
+        'e880ca8f82d9c29f0770092af12332dff5dfa9de48775984ecadf6145c62e56d',
       ],
     },
   },
